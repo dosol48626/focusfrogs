a199af803a94cf425beebbff985a3a36a4a599af
DOSOL\dosol <dosol48626@naver.com>
2024-11-02 20:43:00 +0900

    커밋테스트

---
+++ src/main/java/com/dosol/focusfrogs/FocusfrogsApplication.java
@@ -0,0 +1,13 @@
+package com.dosol.focusfrogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusfrogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FocusfrogsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dosol/focusfrogs/FocusfrogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dosol.focusfrogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusfrogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
